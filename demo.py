4f3af743e8841a92926f6674f12c09d30120b9eb
James Tauber <jtauber@jtauber.com>
2023-04-27 03:00:46 -0400

    more method call examples

--- demo.py
+++ demo.py
@@ -33,3 +33,22 @@ while True:
     if result.get("cursor") is None:
         break
     result = ag.get("app.bsky.graph.getFollows", actor=me, cursor=result["cursor"])
+
+
+result = ag.get("app.bsky.graph.getFollowers", actor=me)
+while True:
+    for follow in result["followers"]:
+        print(follow.get("displayName"), follow.get("handle"), follow.get("viewer"))
+    if result.get("cursor") is None:
+        break
+    result = ag.get("app.bsky.graph.getFollowers", actor=me, cursor=result["cursor"])
+
+
+
+ag.get("com.atproto.repo.describeRepo", repo=me)
+ag.get("com.atproto.repo.listRecords", repo=me, collection="app.bsky.feed.post")
+ag.get("com.atproto.repo.getRecord", repo=me, collection="app.bsky.feed.post", rkey="3jtubanyq5y2w")
+
+# at://did:plc:cvmmvawq5z2qxfhtu3umrx3f/app.bsky.graph.follow/3ju6ejrzybx22
+
+display(ag.get("com.atproto.repo.getRecord", repo=me, collection="app.bsky.graph.follow", rkey="3ju6ejrzybx22"))
